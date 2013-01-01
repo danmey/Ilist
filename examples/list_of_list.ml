f86d7afd09207892adfdd85f1067a594a730c478
Wojciech Meyer <wojciech.meyer@gmail.com>
2013-01-01 22:39:46 +0000

    Another example

---
+++ examples/list_of_list.ml
@@ -0,0 +1,9 @@
+module Type = NList.Type;;
+
+(* NList allows us to define lists of list of the same length *)
+
+(* This will type: *)
+[ [@ 1;2]; [@3;4]; [@4;5]];;
+
+(* but this will not: *)
+[ [@2]; [@3;4]; [@4;5]];;
