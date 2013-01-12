a0659030350f0148678bbc3bfc6d42de7f7a0fe5
Wojciech Meyer <wojciech.meyer@gmail.com>
2013-01-12 22:12:52 +0000

    Patterns in ppx now this works

--- ppx/ppx_ilist.ml
+++ ppx/ppx_ilist.ml
@@ -7,7 +7,9 @@ open Longident
 let mapper =
   object(this)
     inherit Ast_mapper.create as super
+
     val nlst = false
+
     method! expr e =
       match e.pexp_desc with
 
@@ -21,6 +23,15 @@ let mapper =
         { e with pexp_desc = Pexp_construct ({txt = Ldot (Lident "Types", "Cons"); loc},Some ({ e with pexp_desc = Pexp_tuple [this # expr first;this # expr second]}), f)}
 
       | _ -> super # expr e
+
+    method! pat p =
+      match p.ppat_desc with
+      | Ppat_construct ({txt = Lident "[]"; loc}, None, false) when nlst = true ->
+        { p with ppat_desc = Ppat_construct ({txt = Ldot (Lident "Types", "Nil"); loc}, None, false)}
+      | Ppat_construct ({txt = Lident "::"; loc}, (Some ({ppat_desc = Ppat_tuple [first; second]})), f) when nlst = true ->
+        { p with ppat_desc = Ppat_construct ({txt = Ldot (Lident "Types", "Cons"); loc},Some ({ p with ppat_desc = Ppat_tuple [this # pat first;this # pat second]}), f)}
+      | _ -> super # pat p
+
   end
 
 let () = mapper # main
--- ppx_examples/ppx_example.ml
+++ ppx_examples/ppx_example.ml
@@ -1,3 +1,27 @@
 module Types = NList.Types
 let a = [1;2;3;4];;
-let b = NLIST.(1::[1;2;3;4]);;
+let b = NLIST.(1::[2;3]);;
+
+(* Works *)
+NLIST.(match b with
+| x :: y :: z -> ());;
+
+(* Works *)
+NLIST.(match b with
+| [x; y; z] -> ());;
+
+(* Will not compile *)
+(* NLIST.(match b with *)
+(* | [x; y; z; _] -> ());; *)
+
+(* Works *)
+NLIST.(match b with
+| x :: y :: _ -> ());;
+
+(* Works *)
+NLIST.(match b with
+| x :: y :: z :: _ -> ());;
+
+(* Will not compile *)
+(* NLIST.(match b with *)
+(* | x :: y :: z :: _ :: _ -> ());; *)
